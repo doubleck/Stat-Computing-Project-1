6bed465ffa545ebfd49e2b2910edaf26abbbac5f
Jung-Han Wang <doubleck@gmail.com>
2014-10-07 01:56:41 -0400

    I finish Problem 1-1 ~ 1-3.

--- RcodeExam1.R
+++ RcodeExam1.R
@@ -1,5 +1,55 @@
 ##This is R Code for Statistical Computing
 
-a<-1
-b<-2
-c<-3
+## Problem 1
+
+## Load MASS Library
+library (MASS)
+attach (Boston)
+
+x<-Boston$medv
+cv<-sd(x)/mean(x);cv  ##Answer 1-1.
+
+N=200
+n<-length(x);n
+cvb<-numeric(N)
+
+##Bootstrap estimate of standard error of cv
+
+for (k in 1:N) 
+{
+i<-sample(1:n, size=n, replace=TRUE)  
+
+xbar<-mean(x[i])
+s<-sd(x[i])
+cvb[k]<-s/xbar
+}
+
+se.cv<-sd(cvb);se.cv
+hist(cvb,prob=TRUE)
+m<-2000
+cvm<-numeric(m)
+
+for(k in 1:m)
+{
+  i<-sample(1:n, size=n, replace=TRUE)
+  xbar<-mean(x[i])
+  s<-sd(x[i])
+  cvm[k]<-s/xbar
+}
+
+bias<-mean(cvm)-cv;bias
+
+##Bootstrap Percentile confidence interval
+
+alfa<-0.05
+ci<-quantile (cvm,c(alfa/2,1-alfa/2),type=1);ci     ## Answer for 1-2
+
+
+##Calculate Standard error for T Distribution
+
+error<-qt (.975,df=n-1)*s/sqrt(n);error
+
+cit.upper<-mean(cvm)+error;cit.upper
+cit.bot<-mean(cvm)-error;cit.bot     ## Answer for 1-3
+
+

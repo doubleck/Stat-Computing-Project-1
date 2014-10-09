1dba5cdbecae1875cf49a7a318a1cce68088740c
doubleck <hankckh@gmail.com>
2014-10-09 13:51:10 -0400

    Update, but problem 1.3 and 1.4 need to modifired before run.
    "BCA and CVTool" can be re-checked due to use package

--- RcodeExam1.R
+++ RcodeExam1.R
@@ -10,23 +10,12 @@ library (MASS)
 attach (Boston)
 
 x<-Boston$medv
-cv<-sd(x)/mean(x);cv  
+cv<-sd(x)/mean(x);cv ##Answer 1-1.
 
 N=2000
 n<-length(x);n
-cvb<-numeric(N)  ##Answer 1-1.
 
 ##Bootstrap estimate of standard error of cv
-
-for (k in 1:N) 
-{
-i<-sample(1:n, size=n, replace=TRUE)  
-
-xbar<-mean(x[i])
-s<-sd(x[i])
-cvb[k]<-s/xbar
-}
-
 se.cv<-sd(cvb);se.cv 
 hist(cvb,prob=TRUE)
 m<-2000
@@ -39,6 +28,8 @@ for(k in 1:m)
   s<-sd(x[i])
   cvm[k]<-s/xbar
 }
+
+sd(cvm)
 
 mean.cvm<-mean(cvm);mean.cvm 
 
@@ -54,8 +45,9 @@ ci<-quantile (cvm,c(alfa/2,1-alfa/2),type=1);ci     ## Answer for 1-2
 
 error<-qt (.975,df=n-1)*s/sqrt(n);error
 
-cit.upper<-mean(cvm)+error;cit.upper
-cit.bot<-mean(cvm)-error;cit.bot     
+##these code does not compatible with it self, this is along with BOB's code
+cit.upper<-mean(cvm)+qt (.975,df=n-1)*std_err_theta/sqrt(n) ;cit.upper
+cit.bot<-mean(cvm)-qt (.975,df=n-1)*std_err_theta/sqrt(n) ;cit.bot
 
 cit.ci=c(cit.bot,cit.upper);cit.ci
 ## Answer for 1-3
@@ -69,14 +61,75 @@ for (k in 1:p)
   i<-sample (1:n, size=n, replace=TRUE)
   xbar<-mean(x[i])
   minus<-(x[i]-xbar)^2
-  
-  
-  
 }
 
 library(boot)
 
-boot(data=x, statistic=c, R=1000, conf=0.95)
 
-boot.ci(cvm,type="bca")
+f<-function(data,i)
+{
+  i<-sample(1:n, size=n, replace=TRUE)
+  xbar<-mean(x[i])
+  s<-sd(x[i])
+  cvm<-s/xbar
+  
+}
+
+bca.err<-boot(data=x, statistic=f , R=5000)
+
+boot.ci(bca.err,type="bca")  ##Answer 1-4.
+
+
+## Problem 2 --------------------------------------------------
+
+##Load Library(DAAG) as indicated in example 7.18 page 210 in Rizzo's book.
+
+install.packages("DAAG")
+library (DAAG)
+
+##Create 10 equally size folds
+
+install.packages("cvTools")
+
+library(cvTools) #run the above line if you don't have this library
+
+folds.k<- 10 #the number of folds
+
+folds<- cvFolds(NROW(ironslag), K=folds.k)
+
+ironslag$holdoutpred_lin <- rep(0,nrow(ironslag))
+ironslag$holdoutpred_quad <- rep(0,nrow(ironslag))
+ironslag$holdoutpred_exp <- rep(0,nrow(ironslag))
+ironslag$holdoutpred_log <- rep(0,nrow(ironslag))
+
+## Calculate MSE for Linear
+for(i in 1:k){
+  train <- ironslag[folds$subsets[folds$which != i], ] #Set the training set
+  validation <- ironslag[folds$subsets[folds$which == i], ] #Set the validation set
+  new_lin <- lm(magnetic ~ chemical,data=train) #Set linear model 
+  new_quad <- lm(magnetic ~ chemical + I(chemical^2),data=train) #Set Quadratic model 
+  new_exp <- lm(log(magnetic) ~ chemical,data=train) #Set Exponential model 
+  new_log <- lm(log(magnetic) ~ log(chemical),data=train) #Set Log-Log model 
+  
+  newpred_lin <- predict(new_lin,newdata=validation) #Get the predicitons for the validation set
+  newpred_quad <- predict(new_quad,newdata=validation) #Get the predicitons for the validation set
+  newpred_exp <- predict(new_exp,newdata=validation) #Get the predicitons for the validation set
+  newpred_log <- predict(new_log,newdata=validation) #Get the predicitons for the validation set
+  
+  ironslag[folds$subsets[folds$which == i], ]$holdoutpred_lin <- newpred_lin #Put the hold out prediction in the data set for later use
+  ironslag[folds$subsets[folds$which == i], ]$holdoutpred_quad <- newpred_quad #Put the hold out prediction in the data set for later use
+  ironslag[folds$subsets[folds$which == i], ]$holdoutpred_exp <- newpred_exp #Put the hold out prediction in the data set for later use
+  ironslag[folds$subsets[folds$which == i], ]$holdoutpred_log <- newpred_log #Put the hold out prediction in the data set for later use
+}
+ironslag$holdoutpred_exp #Oringinal Dataset with Predictions
+ironslag$holdoutpred_quad #Oringinal Dataset with Predictions
+
+mse.lin<-mean((ironslag$holdoutpred_lin-ironslag$magnetic)^2) ##Calculate MSE for Linear Model
+mse.quad<-mean((ironslag$holdoutpred_quad-ironslag$magnetic)^2) ##Calculate MSE for Linear Model
+mse.exp<-mean((ironslag$holdoutpred_exp-ironslag$magnetic)^2) ##Calculate MSE for Linear Model
+mse.log<-mean((ironslag$holdoutpred_log-ironslag$magnetic)^2) ##Calculate MSE for Linear Model
+
+mse<-c(mse.lin, mse.quad,mse.exp,mse.log);mse
+
+
 
